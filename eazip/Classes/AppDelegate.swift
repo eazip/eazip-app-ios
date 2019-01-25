1e5ba1610201d6bcfc54682b253f8d0f0bd7f14a
Marie <marie@Maries-iMac.local>
2019-01-25 02:31:02 +0100

    [EVOL] - Add custom buttom to first instanciated view

--- eazip/Classes/AppDelegate.swift
+++ eazip/Classes/AppDelegate.swift
@@ -15,7 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let firstScreen = LoginViewController()
+        let navigationController = UINavigationController(rootViewController: firstScreen)
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
--- eazip/Classes/UI/UIComponents/MainActionButton.swift
+++ eazip/Classes/UI/UIComponents/MainActionButton.swift
@@ -8,8 +8,26 @@
 
 import UIKit
 
-@IBDesignable class MainActionButton: UIButton {
-    override func awakeFromNib() {
-       
+class MainActionButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUp()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setUp()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        setUp()
+    }
+    
+    func setUp() {
+        self.backgroundColor = UIColor(named: "eazipDarkBlue")
+        self.setTitleColor(.white, for: .normal)
+        self.layer.shadowColor = UIColor.gray.cgColor
+        self.layer.shadowOpacity = 1
+        self.layer.shadowOffset = CGSize.zero
     }
 }
