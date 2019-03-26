f7a0971226fd1e2e41c9265c9f15211c24a3e4e4
Marie <marie@Maries-iMac.local>
2019-03-26 19:07:59 +0100

    [EVOL] Constraints to tableview

--- Eazip/Classes/UI/UIPages/SewerProfilePage/Layouts/SewerReviewCell.swift
+++ Eazip/Classes/UI/UIPages/SewerProfilePage/Layouts/SewerReviewCell.swift
@@ -13,15 +13,16 @@ class SewerReviewCell: UITableViewCell {
     @IBOutlet weak var reviewTextContent: UILabel!
     @IBOutlet weak var reviewAuthor: UILabel!
     
-    func setSewerReview(review: SewerReview) {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        contentView.frame = UIEdgeInsetsInsetRect(contentView.frame, UIEdgeInsetsMake(0, 0, 0, 0))
         let borderColor : UIColor = UIColor(named: "borderLightGrey")!
         self.layer.masksToBounds = false
         self.layer.borderWidth = 1
         self.layer.cornerRadius = 5
         self.layer.borderColor = borderColor.cgColor
-      
-        reviewTextContent.text = review.textContent
-        reviewAuthor.text = review.author
+        reviewAuthor.font = FontHelper.avenirBlackFontWithSize(size: 13)
     }
 }
 
--- Eazip/Classes/UI/UIPages/SewerProfilePage/Views/SewerProfileController.swift
+++ Eazip/Classes/UI/UIPages/SewerProfilePage/Views/SewerProfileController.swift
@@ -15,23 +15,21 @@ class SewerProfileController: UIViewController {
     @IBOutlet var descriptionContent : UILabel?
     @IBOutlet weak var sewerPictureView : UIImageView!
     @IBOutlet var showAvailabilityBtn : ColoredActionButton?
-    @IBOutlet var lastWorksTitle : UILabel?
     @IBOutlet var reviewsTitle : UILabel?
     @IBOutlet var tableView : UITableView?
+    
     var sewerReviewsList: [SewerReview] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpSewerPicture()
         setUpDescription()
-        setUpLastWorks()
         sewerReviewsList = setUpReviews()
-        tableView?.delegate = self
+        
         tableView?.dataSource = self
         tableView?.rowHeight = UITableViewAutomaticDimension
-        tableView?.estimatedRowHeight = UITableViewAutomaticDimension
-        tableView?.contentInset = UIEdgeInsetsMake(20.0, 0.0, 0.0, 0.0)
-    
+        tableView?.estimatedRowHeight = 600
+        
         showAvailabilityBtn?.setTitle("Voir ses disponibilités", for: .normal)
     }
     
@@ -47,22 +45,22 @@ class SewerProfileController: UIViewController {
         let sewerPicture = UIImage(named:"sewerPicture2")!
         sewerPictureView.image = sewerPicture
         
-        let view = UIView(frame: sewerPictureView.frame)
-        view.translatesAutoresizingMaskIntoConstraints = false
-        sewerPictureView.addSubview(view)
-        sewerPictureView.bringSubview(toFront: view)
-        view.frame.size.width = screenWidth
+        let gradientView = UIView(frame: sewerPictureView.frame)
+        gradientView.translatesAutoresizingMaskIntoConstraints = false
+        sewerPictureView.addSubview(gradientView)
+        sewerPictureView.bringSubview(toFront: gradientView)
+        gradientView.frame.size.width = screenWidth
        
         let gradient = CAGradientLayer()
-        gradient.frame = view.frame
+        gradient.frame = gradientView.frame
         gradient.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
         gradient.locations = [0.0, 1.0]
         
-        view.layer.insertSublayer(gradient, at: 0)
+        gradientView.layer.insertSublayer(gradient, at: 0)
 
-        view.addSubview(sewerName!)
-        view.bringSubview(toFront: sewerName!)
-        view.addSubview(rankLabel!)
+        gradientView.addSubview(sewerName!)
+        gradientView.bringSubview(toFront: sewerName!)
+        gradientView.addSubview(rankLabel!)
         view.bringSubview(toFront: rankLabel!)
         sewerName?.bottomAnchor.constraint(equalTo: sewerPictureView.bottomAnchor, constant: -45).isActive = true
         rankLabel?.bottomAnchor.constraint(equalTo: sewerPictureView.bottomAnchor, constant: -15).isActive = true
@@ -77,10 +75,6 @@ class SewerProfileController: UIViewController {
     func setUpDescription() {
         descriptionTitle?.text = "Description"
         descriptionContent?.text = "Denique Antiochensis ordinis vertices sub uno elogio iussit occidi ideo efferatus, quod ei celebrari vilitatem intempestivam urgenti, cum inpenderet inopia, gravius rationabili responderunt; et perissent ad unum ni comes orientis tunc Honoratus fixa constantia restitisset."
-    }
-    
-    func setUpLastWorks() {
-        lastWorksTitle?.text = "Dernières retouches"
     }
     
     func setUpReviews() -> [SewerReview] {
@@ -98,21 +92,15 @@ class SewerProfileController: UIViewController {
 }
 
 extension SewerProfileController: UITableViewDataSource, UITableViewDelegate {
-    
-    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
-        return UITableViewAutomaticDimension
-    }
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return sewerReviewsList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let sewerReview = sewerReviewsList[indexPath.row]
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: "SewerReviewCell") as! SewerReviewCell
-        
-        cell.setSewerReview(review: sewerReview)
+        let review = sewerReviewsList[indexPath.row]
+        cell.reviewTextContent.text = review.textContent
+        cell.reviewAuthor.text = sewerReviewsList[indexPath.row].author
         
         return cell
     }
