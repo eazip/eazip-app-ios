076432c9d89d9bc258385cf077d9edccc8479f71
Marie <marie@Maries-iMac.local>
2019-03-29 18:09:26 +0100

    [EVOL] Division in different sections in sewerProfile CollectionView

--- Eazip/Classes/UI/UIPages/SewerProfilePage/Views/SewerProfilePage.swift
+++ Eazip/Classes/UI/UIPages/SewerProfilePage/Views/SewerProfilePage.swift
@@ -39,12 +39,13 @@ class SewerProfilePageController: UIViewController, UICollectionViewDataSource, 
         //Sections
         let pictureView = setUpPictureView(picture: dataSewer.sewerPicture, name: dataSewer.sewerName, rating: dataSewer.sewerRating)
         let descriptionView = setUpDescriptionView(biography: dataSewer.sewerBiography)
-        reviewsViews = setUpReviewView(reviews: dataSewer.sewerReviews as! [SewerReview])
         
         //Init sections
         profileViewCategories.append(pictureView)
         profileViewCategories.append(descriptionView)
-        profileViewCategories.append(reviewsViews)
+        
+        reviewsViews = setUpReviewView(reviews: dataSewer.sewerReviews)
+
     }
     
     func setUpPictureView(picture: UIImage, name: String, rating: Int) -> [String: Any] {
@@ -66,38 +67,66 @@ class SewerProfilePageController: UIViewController, UICollectionViewDataSource, 
     }
     
     func setUpReviewView(reviews: [SewerReview]) -> [SewerReview] {
-        sewerProfileCollectionView?.register(UINib(nibName:"ReviewViewCell", bundle: nil), forCellWithReuseIdentifier: "ReviewViewCell")
+        for _ in reviews {
+            sewerProfileCollectionView?.register(UINib(nibName:"ReviewViewCell", bundle: nil), forCellWithReuseIdentifier: "ReviewViewCell")
+        }
         
         return reviews
     }
     
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return profileViewCategories.count
+        return (section == 0) ? profileViewCategories.count : reviewsViews.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let categoryIndex = indexPath.row
-        let categoryData = profileViewCategories[categoryIndex]
-        
-        switch(categoryIndex) {
-            case 0:
-                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PictureViewCell", for: indexPath) as! PictureViewCell
-                cell.setData()
-                return cell
-            case 1:
-                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DescriptionViewCell", for: indexPath) as! DescriptionViewCell
-                cell.setData()
-                return cell
-            case 2:
-                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ReviewViewCell", for: indexPath) as! ReviewViewCell
-                cell.setData()
-                return cell
-            default:
-                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PictureViewCell", for: indexPath) as! PictureViewCell
-                cell.setData()
-                return cell
+        if indexPath.section == 0 {
+            let categoryIndex = indexPath.row
+            let categoryData = profileViewCategories[categoryIndex]
+            
+            switch(categoryIndex) {
+                case 0:
+                    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PictureViewCell", for: indexPath) as! PictureViewCell
+                    cell.setData()
+                    return cell
+                case 1:
+                    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DescriptionViewCell", for: indexPath) as! DescriptionViewCell
+                    cell.setData()
+                    return cell
+                default:
+                    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PictureViewCell", for: indexPath) as! PictureViewCell
+                    cell.setData()
+                    return cell
+            }
+        } else {
+            let reviewIndex = indexPath.row
+            let reviewData = reviewsViews[reviewIndex]
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ReviewViewCell", for: indexPath) as! ReviewViewCell
+            
+            cell.setData()
+            return cell
         }
+    }
     
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+       
+        let sectionTitle = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "profileSectionTitle", for: indexPath) as! ProfileSectionTitle
+        sectionTitle.setData()
         
+        return sectionTitle
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        switch section {
+        case 0:
+            return CGSize(width: 0, height: 0)
+        case 1:
+            return CGSize(width: collectionView.bounds.width, height: 70)
+        default:
+            return CGSize(width: collectionView.bounds.width, height: 70)
+        }
     }
 }
