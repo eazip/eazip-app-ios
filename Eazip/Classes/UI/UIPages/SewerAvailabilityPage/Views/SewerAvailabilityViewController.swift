e6cba8df560e9ecd0300533be557542e9a8c634e
Marie <marie@Maries-iMac.local>
2019-04-04 11:15:44 +0200

    [EVOL] Init viewcontroller with view in storyboard

---
+++ Eazip/Classes/UI/UIPages/SewerAvailabilityPage/Views/SewerAvailabilityViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SewerAvailabilityViewController.swift
+//  Eazip
+//
+//  Created by Marie on 04/04/2019.
+//  Copyright © 2019 Eazip. All rights reserved.
+//
+
+import UIKit
+
+class SewerAvailabilityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
