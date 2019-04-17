e0c622f7ce03759070169d99561bfe45e5551b90
Marie <marie@Maries-iMac.local>
2019-04-17 08:14:31 +0200

    [EVOL] - init new view controller for appointment confirmation

---
+++ Eazip/Classes/UI/UIPages/ConfirmAppointmentPage/Views/ConfirmAppointmentViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ConfirmAppointmentViewController.swift
+//  Eazip
+//
+//  Created by Marie on 17/04/2019.
+//  Copyright © 2019 Eazip. All rights reserved.
+//
+
+import UIKit
+
+class ConfirmAppointmentViewController: UIViewController {
+
+    @IBOutlet weak var appointmentConfirmationCollectionView: UICollectionView!
+    @IBOutlet weak var confirmAppointmentButton: ColoredActionButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
