18eef5b2075b361bb1c8e5e111fb7e631dc129b8
Marie <marie@Maries-iMac.local>
2019-05-03 11:00:05 +0200

    [EVOL] Init viewcontroller

---
+++ Eazip/Classes/UI/UIPages/AppointmentDetailsPage/Views/AppointmentDetailsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  AppointmentDetailsViewController.swift
+//  Eazip
+//
+//  Created by Marie on 03/05/2019.
+//  Copyright © 2019 Eazip. All rights reserved.
+//
+
+import UIKit
+
+class AppointmentDetailsViewController: UIViewController {
+
+    @IBOutlet weak var bannerWrapper: UIView!
+    @IBOutlet weak var bannerLabel: UILabel!
+    @IBOutlet weak var appointmentDetailsCollectionView: UICollectionView!
+    @IBOutlet weak var cancellationWrapper: UIView!
+    @IBOutlet weak var cancellationButton: UIButton!
+    @IBOutlet weak var cancellationLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
