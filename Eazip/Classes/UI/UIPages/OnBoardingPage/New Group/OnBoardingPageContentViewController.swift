da0b0f079e67f41447cedc9d02aac5a94ef94dcd
Marie <marie@Maries-iMac.local>
2019-05-28 01:03:09 +0200

    [EVOL] - Onboardin WIP : constraints to fix with button to start app navigation

---
+++ Eazip/Classes/UI/UIPages/OnBoardingPage/New Group/OnBoardingPageContentViewController.swift
@@ -0,0 +1,54 @@
+//
+//  OnBoardingPageContentViewController.swift
+//  Eazip
+//
+//  Created by Marie on 27/05/2019.
+//  Copyright © 2019 Eazip. All rights reserved.
+//
+
+import UIKit
+
+class OnBoardingPageContentViewController: UIViewController {
+    @IBOutlet var mainView: UIView!
+    @IBOutlet weak var logoFerrandinetitle: EazipLabel!
+    @IBOutlet weak var imageWrapperView: UIView!
+    @IBOutlet weak var pageMainText: UILabel!
+    @IBOutlet weak var pageLabel: EazipLabel!
+    @IBOutlet weak var pageImage: UIImageView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    var pageIndex: Int!
+    var dataPageLabel : String!
+    var dataPageMainText : String!
+    var dataPageImage : UIImage!
+    var totalNumberOfPages : Int!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        setData()
+    }
+    
+    func setupUI() {
+        mainView.setEazipGradient(colors: [UIColor(named: "peachColor")!, UIColor(named: "lightPeachColor")!])
+        logoFerrandinetitle.textColor = .white
+        imageWrapperView.layer.cornerRadius = 20
+        pageLabel.textColor = .white
+        pageMainText.textColor = .white
+        pageLabel.textAlignment = .center
+        pageMainText.textAlignment = .center
+    }
+    
+    func setData() {
+        pageImage.image = dataPageImage
+        pageLabel.text = dataPageLabel
+        pageMainText.text = dataPageMainText
+        pageControl.numberOfPages = totalNumberOfPages
+        pageControl.currentPage = pageIndex
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
