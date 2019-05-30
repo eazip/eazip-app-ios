73ec9c9a47d6009409345b2617eac306cd93f139
Quentin Michalet <quentin.michalet90700@gmail.com>
2019-05-30 12:10:45 +0200

    removed old fake data

--- Eazip/Classes/UI/UIPages/ClothPickingPage/Views/ClothsPickingViewController.swift
+++ Eazip/Classes/UI/UIPages/ClothPickingPage/Views/ClothsPickingViewController.swift
@@ -20,59 +20,51 @@ class ClothsPickingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        clothsList = createArray()
+        getCloths()
         setUpTitleScreen()
         initClothsTableView()
         setUpValidationButton()
         makeNextStepUnavailable()
     }
     
-    func createArray() -> [ClothItem] {
-        var tempClothList: [ClothItem] = []
+    func getCloths() {
+        ApiClothsHelper() { clothList, error in
+            if clothList != nil {
+                self.clothsList.append(contentsOf: clothList!)
+            }
+        }
+    }
+    
+    typealias Apicompletion = (_ clothList: [ClothItem]?, _ errorString: String? ) -> Void
+    
+    func ApiClothsHelper (completion: Apicompletion?) {
+        let url = URL(string: "http://ec2-35-180-118-48.eu-west-3.compute.amazonaws.com/clothes")
+        let session = URLSession.shared
         
-            if let url = URL(string: "http://ec2-35-180-118-48.eu-west-3.compute.amazonaws.com/clothes") {
-                let session = URLSession.shared
-                session.dataTask(with: url) { (data, response, error) in
-        
-                    if let data = data {
-                        do {
-                            if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
-                                if let clothArray = json["data"] as? NSArray {
-                                    for cloth in clothArray as! [Dictionary<String, AnyObject>] {
-                                        let id = cloth["id"] as! Int
-                                        let label = cloth["label"] as! String
-                                        var icon = ""
-                                        if cloth["label"] as! String == "Robe" {
-                                            icon = "robe"
-                                        } else if cloth["label"] as! String == "Pantalon" {
-                                            icon = "pantalon"
-                                        } else if cloth["label"] as! String == "T-Shirt" {
-                                            icon = "t-shirt"
-                                        } else if cloth["label"] as! String == "Jupe" {
-                                            icon = "jupe"
-                                        } else if cloth["label"] as! String == "Short" {
-                                            icon = "pantalon"
-                                        } else if cloth["label"] as! String == "Pull-Over" {
-                                            icon = "pullover"
-                                        } else if cloth["label"] as! String == "Blazer" {
-                                            icon = "veste"
-                                        } else if cloth["label"] as! String == "Manteau" {
-                                            icon = "manteau"
-                                        }
-                                        
-                                        tempClothList.append(ClothItem(cloth_id: id, icon: UIImage(named: icon)!, title: label, selected: false))
-                                    }
-                                }
+        session.dataTask(with: url!) { (data, response, error)  in
+            var tempClothList: [ClothItem] = []
+            
+            if let data = data {
+                do {
+                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                        if let clothArray = json["data"] as? NSArray {
+                            for cloth in clothArray as! [Dictionary<String, AnyObject>] {
+                                print(cloth)
+                                let id = cloth["id"] as! Int
+                                let label = cloth["label"] as! String
+                                
+                                tempClothList.append(ClothItem(cloth_id: id, icon: UIImage(named: "robe")!, title: label, selected: false))
+                                print(tempClothList)
                             }
-        
-                        } catch {
-                            print(error)
+                            completion?(tempClothList, nil)
                         }
                     }
                     
-                }.resume()
+                } catch {
+                    completion?(nil, error.localizedDescription)
+                }
             }
-        return tempClothList
+            }.resume()
     }
     
     func setUpTitleScreen() {
@@ -126,6 +118,7 @@ class ClothsPickingViewController: UIViewController {
     func nextStep() -> Void {
         if navigationAllowed {
             goToScreen(identifier: "WorksByClothViewController")
+            print("selectedClothes", selectedClothes)
         }
     }
 }
