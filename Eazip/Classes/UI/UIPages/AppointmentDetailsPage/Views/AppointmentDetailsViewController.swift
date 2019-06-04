78e6c267fce81202c45c33c0875f10ec7e63c345
Quentin Michalet <quentin.michalet90700@gmail.com>
2019-06-05 00:42:53 +0200

    formatted month value to return proper string

--- Eazip/Classes/UI/UIPages/AppointmentDetailsPage/Views/AppointmentDetailsViewController.swift
+++ Eazip/Classes/UI/UIPages/AppointmentDetailsPage/Views/AppointmentDetailsViewController.swift
@@ -34,12 +34,12 @@ class AppointmentDetailsViewController: UIViewController {
     }
     
     func initAppointmentData() -> Appointment {
-        return Appointment(sewerFirstName: currentProfile.sewerFirstName, sewerLastName: currentProfile.sewerLastName, sewerRating: currentProfile.sewerRating, sewerBio: currentProfile.sewerBio, sewerStreet: currentProfile.sewerStreet, sewerCity: currentProfile.sewerCity, day: appointmentDate?.currentDay ?? 0, month: "Juin", year: appointmentDate?.currentYear ?? 0, hour: appointmentDate?.currentHour ?? 0)
+        return Appointment(sewerFirstName: currentProfile.sewerFirstName, sewerLastName: currentProfile.sewerLastName, sewerRating: currentProfile.sewerRating, sewerBio: currentProfile.sewerBio, sewerStreet: currentProfile.sewerStreet, sewerCity: currentProfile.sewerCity, day: appointmentDate?.currentDay ?? 0, month: formatMonthValue(monthCode: appointmentDate?.currentMonth ?? 0), year: appointmentDate?.currentYear ?? 0, hour: appointmentDate?.currentHour ?? 0)
     }
     
     func setUpHeaderView() {
         headerviewLabel.textAlignment = .center
-        headerviewLabel.text = String(appointmentDate?.currentDay ?? 0) + " " + "Juin" + " " + String(appointmentDate?.currentYear ?? 0)
+        headerviewLabel.text = String(appointmentDate?.currentDay ?? 0) + " " + formatMonthValue(monthCode: appointmentDate?.currentMonth ?? 0) + " " + String(appointmentDate?.currentYear ?? 0)
     }
     
     func setUpBanner() {
@@ -110,5 +110,36 @@ class AppointmentDetailsViewController: UIViewController {
     
     func setUpCancellationButton() {
         cancellationButton.setTitle("Annuler la retouche", for: .normal)
+    }
+    
+    func formatMonthValue(monthCode: Int) -> String {
+        switch monthCode {
+        case 1:
+            return "Janvier"
+        case 2:
+            return "Février"
+        case 3:
+            return "Mars"
+        case 4:
+            return "Avril"
+        case 5:
+            return "Mai"
+        case 6:
+            return "Juin"
+        case 7:
+            return "Juillet"
+        case 8:
+            return "Août"
+        case 9:
+            return "Septembre"
+        case 10:
+            return "Octobre"
+        case 11:
+            return "Novembre"
+        case 12:
+            return "Décembre"
+        default:
+            return "Janvier"
+        }
     }
 }
